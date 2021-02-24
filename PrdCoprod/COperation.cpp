36b769b7d7836660847e3900a5006afd1da1aa88
Yohan nouet <yohan.nouet@gmail.com>
2021-02-24 16:42:38 +0100

    cration classe Operation

--- PrdCoprod/COperation.cpp
+++ PrdCoprod/COperation.cpp
@@ -10,3 +10,223 @@ COperation::COperation()
 COperation::~COperation()
 {
 }
+
+int COperation::getIdCplex()
+{
+	return idCplex;
+}
+
+void COperation::setIdCplex(int idParam)
+{
+	idCplex = idParam;
+}
+
+int COperation::getIdReference()
+{
+	return idReference;
+}
+
+void COperation::setIdReference(int idParam)
+{
+	idReference = idParam;
+}
+
+string COperation::getRefOperation()
+{
+	return refOperation;
+}
+
+void COperation::setRefOperation(string str)
+{
+	refOperation = str;
+}
+
+string COperation::getDescOperation()
+{
+	return descOperation;
+}
+
+void COperation::setDescOperation(string str)
+{
+	descOperation = str;
+}
+
+string COperation::getVersionPMS()
+{
+	return versionPMS;
+}
+
+void COperation::setVersionPMS(string str)
+{
+	versionPMS = str;
+}
+
+bool COperation::getVersionPMSEnCours()
+{
+	return versionPMSEnCours;
+}
+
+void COperation::setVersionPMSEnCours(bool param)
+{
+	versionPMSEnCours = param;
+}
+
+string COperation::getTypeOperationPartService()
+{
+	return typeOperationPartService;
+}
+
+void COperation::setTypeOperationPartService(string str)
+{
+	typeOperationPartService = str;
+}
+
+string COperation::getCycle()
+{
+	return cycle;
+}
+
+void COperation::setCycle(string str)
+{
+	cycle = str;
+}
+
+string COperation::getStfOpePart()
+{
+	return stfOpePart;
+}
+
+void COperation::setStfOpePart(string str)
+{
+	stfOpePart = str;
+}
+
+string COperation::getStatutOpeRef()
+{
+	return statutOpeRef;
+}
+
+void COperation::setStatutOpeRef(string str)
+{
+	statutOpeRef = str;
+}
+
+string COperation::getLcnRacine()
+{
+	return lcnRacine;
+}
+
+void COperation::setLcnRacine(string str)
+{
+	lcnRacine = str;
+}
+
+string COperation::getLcnArticle()
+{
+	return lcnArticle;
+}
+
+void COperation::setLcnArticle(string str)
+{
+	lcnArticle = str;
+}
+
+string COperation::getRefDocMaintenance()
+{
+	return refDocMaintenance;
+}
+
+void COperation::setRefDocMaintenance(string str)
+{
+	refDocMaintenance = str;
+}
+
+int COperation::getRang()
+{
+	return rang;
+}
+
+void COperation::setRang(int idParam)
+{
+	rang = idParam;
+}
+
+string COperation::getDateDebutApplicabilite()
+{
+	return dateDebutApplicabilite;
+}
+
+void COperation::setDateDebutApplicabilite(string str)
+{
+	dateDebutApplicabilite = str;
+}
+
+string COperation::getDateFinApplicabilite()
+{
+	return dateFinApplicabilite;
+}
+
+void COperation::setDateFinApplicabilite(string str)
+{
+	dateFinApplicabilite = str;
+}
+
+string COperation::getSEF()
+{
+	return sef;
+}
+
+void COperation::setSEF(string str)
+{
+	sef = str;
+}
+
+string COperation::getCadence()
+{
+	return cadence;
+}
+
+void COperation::setCadence(string str)
+{
+	cadence = str;
+}
+
+string COperation::getTypeOperation()
+{
+	return typeOperation;
+}
+
+void COperation::setTypeOperation(string str)
+{
+	typeOperation = str;
+}
+
+string COperation::getLibelleTypeOperation()
+{
+	return libelleTypeOperation;
+}
+
+void COperation::setLibelleTypeOperation(string str)
+{
+	libelleTypeOperation = str;
+}
+
+int COperation::getNbReferentielsApplicables()
+{
+	return nbReferentielsApplicables;
+}
+
+void COperation::setNbReferentielsApplicables(int i)
+{
+	nbReferentielsApplicables = i;
+}
+
+string COperation::getDeclinaisonMR()
+{
+	return declinaisonMR;
+}
+
+void COperation::setDeclinaisonMR(string str)
+{
+	declinaisonMR = str;
+}
--- PrdCoprod/COperation.h
+++ PrdCoprod/COperation.h
@@ -18,28 +18,113 @@ using namespace std;
 class COperation
 {
 private:
-	int idCplex;
-	int idReference;
-	string refOperation;
-	string descOperation;
-	string versionPMS;
-	bool versionPMSEnCours;
-	string typeOperation;
-	string cycle; // peut être transformer en enumeration MODULAIRE REVISION CORRECTIF...
-	string STFOpePart;
-	string statutOpeRef;
-	string LCNRacine;
-	string LCNArticle;
-	string refDocMaintenance;
-	int rang;
-	string
-
-
-
+	int idCplex;					/*!< identifiant CPlex*/
+	int idReference;				/*!< champ fichier csv : Id Op. Ref*/
+	string refOperation;			/*!< champ fichier csv : Opération Ref*/
+	string descOperation;			/*!< champ fichier csv : Description Op. Ref*/
+	string versionPMS;				/*!< champ fichier csv : Version PMS Op.Ref */
+	bool versionPMSEnCours;			/*!< champ fichier csv : Version PMS en cours*/
+	string typeOperationPartService;/*!< champ fichier csv : Type d'Operation Part/Service */
+	string cycle;					/*!< champ fichier csv : Cycle */		// peut être transformer en enumeration MODULAIRE REVISION CORRECTIF...
+	string stfOpePart;				/*!< champ fichier csv : STF / Ope. Part.*/
+	string statutOpeRef;			/*!< champ fichier csv : Statut Op. Ref*/
+	string lcnRacine;				/*!< champ fichier csv : LCN Racine Op. Ref*/
+	string lcnArticle;				/*!< champ fichier csv : LCN Article Op. Ref */
+	string refDocMaintenance;		/*!< champ fichier csv : Reference document Maintenance*/
+	int rang;						/*!< champ fichier csv : Rang */
+	string dateDebutApplicabilite;	/*!< champ fichier csv : Date debut applicabilité Op. Ref */		//TODO changer pour un type date
+	string dateFinApplicabilite;	/*!< champ fichier csv : Date fin applicabilité Op. Ref */		//TODO changer pour un type date
+	string sef;						/*!< champ fichier csv : SEF */
+	string cadence;					/*!< champ fichier csv : Cadence Op. Ref */
+	string typeOperation;			/*!< champ fichier csv : Type Op. Ref */
+	string libelleTypeOperation;	/*!< champ fichier csv : Libelle Type Op. Ref */
+	int nbReferentielsApplicables;	/*!< champ fichier csv : Nb Referentiels Applicables */
+	string declinaisonMR;			/*!< champ fichier csv : Declinaison MR */
 
 
 public:
+
+	/*!
+	*  \brief Constructeur
+	*
+	*  Constructeur par defaut de la classe COperation
+	*/
 	COperation();
+
+
+	/*!
+	*  \brief Destructeur
+	*
+	*  Destructeur de la classe CChantier
+	*/
 	~COperation();
+
+	//////////// GETTERS AND SETTERS ///////////////////
+
+	int getIdCplex();
+	void setIdCplex(int idParam);
+
+	int getIdReference();
+	void setIdReference(int idParam);
+
+	string getRefOperation();
+	void setRefOperation(string str);
+
+	string getDescOperation();
+	void setDescOperation(string str);
+
+	string getVersionPMS();
+	void setVersionPMS(string str);
+
+	bool getVersionPMSEnCours();
+	void setVersionPMSEnCours(bool param);
+
+	string getTypeOperationPartService();
+	void setTypeOperationPartService(string str);
+
+	string getCycle();
+	void setCycle(string str);
+
+	string getStfOpePart();
+	void setStfOpePart(string str);
+
+	string getStatutOpeRef();
+	void setStatutOpeRef(string str);
+
+	string getLcnRacine();
+	void setLcnRacine(string str);
+
+	string getLcnArticle();
+	void setLcnArticle(string str);
+
+	string getRefDocMaintenance();
+	void setRefDocMaintenance(string str);
+
+	int getRang();
+	void setRang(int idParam);
+
+	string getDateDebutApplicabilite();
+	void setDateDebutApplicabilite(string str);
+
+	string getDateFinApplicabilite();
+	void setDateFinApplicabilite(string str);
+
+	string getSEF();
+	void setSEF(string str);
+
+	string getCadence();
+	void setCadence(string str);
+
+	string getTypeOperation();
+	void setTypeOperation(string str);
+
+	string getLibelleTypeOperation();
+	void setLibelleTypeOperation(string str);
+
+	int getNbReferentielsApplicables();
+	void setNbReferentielsApplicables(int i);
+
+	string getDeclinaisonMR();
+	void setDeclinaisonMR(string str);
 };
 
